de189d135ed4255716322bc8347b4b179c169d37
Lautaro <lautaro2205@gmail.com>
2025-02-06 18:47:17 -0300

    Creando css y jsx del componente de twitter

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
+  const dravik = { isFollowing: true, userName: 'dravik' }
+  const vik = { isFollowing: false, userName: 'vikito' }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <section className='App'>
+      <TwitterFollowCard isFollowing userName="midudev">
+        Miguel Ángel
+      </TwitterFollowCard>
+      <TwitterFollowCard isFollowing={false} userName="midudev">
+        Lautaro Manuel
+      </TwitterFollowCard>
+      <TwitterFollowCard {...dravik}>
+        Draco Malote
+      </TwitterFollowCard>
+      <TwitterFollowCard {...vik}>
+        Vikito
+      </TwitterFollowCard>
+    </section >
   )
 }
 
-export default App
